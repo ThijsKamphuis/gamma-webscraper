454ca8edc89d43c195bf1d0a1444fe41b720a2f0
Thijs Kamphuis <thijskamphuis@icloud.com>
2024-02-14 13:16:48 +0100

    added dimension extractor

--- src/api.js
+++ src/api.js
@@ -13,24 +13,40 @@ async function scrapeSite(artnr) {
         .catch(err => {
             throw new Error(String(err.response.status));
         });
-
     const $ = cheerio.load(data);
 
-    const result = {};
-    result.name = ($('h1').text());
+    const { name, size } = getDimensions(($('h1').text()));
+
+    let result = {};
+    result.name = name;
+    result.dimensions = size;
     result.img = ($('img.product-main-image').attr('data-src'));
     result.ean = ($(`div[data-product-code="${artnr}"]`).attr('data-ean'));
-
     return result;
-
 }
 
-app.get("/artinfo", async (req, res) => {
+function getDimensions(name) {
+    const dRegex = [
+        /\d+x\d+x\d+\s*cm/g, // _x_x_ cm
+        /\d+x\d+\s*cm/g, // _x_ cm
+    ];
+    let size = null;
+    for (const regex of dRegex) {
+        const match = name.match(regex);
+        if (match) {
+            size = match[0];
+            name = name.replace(size, "").trim();
+            break;
+        }
+    }
+    return { name, size };
+}
+
+app.get('/artinfo', async (req, res) => {
     const {artnr} = req.query;
     try {
         res.send(await scrapeSite(artnr));
     } catch (err) {
-
         res.sendStatus(err.message);
     }
 });
